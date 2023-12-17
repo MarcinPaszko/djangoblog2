6cfaddf0bfc1b0d1ce78dbb3c2e79be4891be4f9
crysehd <cryshed@gmail.com>
2023-12-17 12:06:30 +0100

    Koncówka

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y1#n7dqz@zaai1y($s8vl&3t_7tk+^7u8$=bx(5eja1j-#0cby
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+#TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ['127.0.0.1', ' .pythonanywhere.com','crysehd.pythonanywhere.com']
 
 
@@ -118,7 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog', 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
